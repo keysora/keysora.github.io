8ca1b46f7fb08e835c22f1f24dcc7e35162bca1f
keysora <keyfralicia@gmail.com>
2025-04-22 22:48:26 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,15 @@
+require('dotenv').config(); // Загружает переменные из .env
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
